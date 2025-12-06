bce15fd9236257c571696e00e76d748fed597de2
whoIsBeto <whoisbeto@gmail.com>
2025-12-05 21:00:54 -0600

    Fix release_date migrations

--- print_migrations.py
+++ print_migrations.py
@@ -1,11 +1,25 @@
-import psycopg2
 import os
+import psycopg2
 
-conn = psycopg2.connect(os.environ["DATABASE_URL"])
+user = os.environ["PG_USER"]
+password = os.environ["PG_PASSWORD"]
+host = os.environ["PG_HOST"]
+port = os.environ["PG_PORT"]
+db = os.environ["PG_DB"]
+
+url = f"postgresql://{user}:{password}@{host}:{port}/{db}"
+
+print("Connecting to:", url)
+
+conn = psycopg2.connect(url)
 cur = conn.cursor()
 
 cur.execute("SELECT app, name FROM django_migrations ORDER BY app, name;")
 rows = cur.fetchall()
 
+print("\n--- MIGRATIONS IN DATABASE ---")
 for app, name in rows:
-    print(f"{app}: {name}")
+    print(f"{app} → {name}")
+
+cur.close()
+conn.close()
