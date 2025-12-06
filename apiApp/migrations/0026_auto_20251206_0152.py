fcdfb1dc74356f612a617c4436e2c420b40ed0f7
whoIsBeto <whoisbeto@gmail.com>
2025-12-05 20:32:35 -0600

    Fix release_date migrations

--- apiApp/migrations/0026_auto_20251206_0152.py
+++
@@ -1,8 +0,0 @@
-from django.db import migrations
-
-class Migration(migrations.Migration):
-
-    dependencies = [
-        ('apiApp', '0023_record_items_inside_alter_record_release_date'),
-    ]
-
